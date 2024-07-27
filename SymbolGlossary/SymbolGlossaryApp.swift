747f98879ede0e6b387c701c8b7c7974f8e2d33b
Matthew Auciello <matt.auciello1999@gmail.com>
2024-07-27 21:46:04 +1000

    Initial Commit

---
+++ SymbolGlossary/SymbolGlossaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolGlossaryApp.swift
+//  SymbolGlossary
+//
+//  Created by Matthew Auciello on 27/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct SymbolGlossaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
